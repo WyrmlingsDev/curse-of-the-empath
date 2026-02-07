7530c9c00ea2e0ee0220bfdff930802e5ceb337e
diegelj08 <101020346+diegelj08@users.noreply.github.com>
2026-02-07 16:35:19 -0500

    Added export tags to trigger
    Added export tags for group, name and scripts

--- scripts/empty_trigger.gd
+++ scripts/empty_trigger.gd
@@ -2,14 +2,24 @@ extends Area2D
 
 class_name GameTrigger
 
+#Export Variables
+@export_group("Trigger Settings")
+@export var trigger_name: String = "DefaultTrigger"
+@export var logic_script: GDScript
+
 #Initializes function in current Node
 func _ready():
 	self.body_entered.connect(_on_body_entered)
 
 #Triggers event when player enters
-func _on_body_entered(body: CharacterBody2D):
+func _on_body_entered(_body: CharacterBody2D):
 	print("Player Detected")
-	#Space for triggered event
-	
-	#Trigger self delete
+	#Loads script from side panel
+	if logic_script:
+		var script_instance = logic_script.new()
+		#Checks for run method in loaded script
+		if script_instance.has_method("run"):
+			script_instance.run()
+
+	#Self deletes trigger
 	queue_free()
