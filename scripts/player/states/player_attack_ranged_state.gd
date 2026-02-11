1d3f7b28ca604012bc4dae8b771e249da786bdb4
flrp <flrping7@gmail.com>
2026-02-11 00:11:50 -0500

    feat: player projectile

--- scripts/player/states/player_attack_ranged_state.gd
+++ scripts/player/states/player_attack_ranged_state.gd
@@ -2,13 +2,20 @@ extends PlayerState
 
 class_name PlayerAttackRangedState
 
+@onready var arrow = preload("res://scenes/projectile/arrow.tscn")
+
 func enter(prev_state: PlayerState) -> void:
 	player.animation.scale.x = 0.3
 	player.animation.scale.y = 0.3
 	player.animation.play("ranged")
 	await player.animation.animation_finished
+	
+	var new_arrow = arrow.instantiate();
+	
+	# this was written at 12 am on a school night...
+	new_arrow.global_position.x = player.global_position.x + (player.facing_direction.x * 200)
+	new_arrow.global_position.y = player.global_position.y - 180
+	new_arrow.velocity = player.facing_direction * 1200
+	get_tree().current_scene.add_child(new_arrow)
+	
 	player._set_state("idle")
-	pass
-
-func physics_update(delta: float) -> void:
-	pass
